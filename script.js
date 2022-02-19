bb8d39ce6cb30416beed67ac1676c31fa66c41ac
diademiemi <emilia@diademiemi.me>
2022-02-19 01:40:14 +0100

    Try to detect mobile devices

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let button = document.getElementById("collapse")
 let body = document.getElementById("body")
 
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+    body.classList.add("contracted")
+}
 button.addEventListener("click", e => {
     body.classList.toggle("contracted")
 })
