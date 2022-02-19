70d45d890914f5ff1b9e2db7decec8b8bfd2f151
diademiemi <emilia@diademiemi.me>
2022-02-19 14:50:14 +0100

    Check for aspect ratio instead of user agent

--- resources/script.js
+++ resources/script.js
@@ -2,12 +2,14 @@ let button = document.getElementById("collapse")
 let sidebar = document.getElementsByClassName("sidebar")[0]
 let body = document.getElementById("body")
 
-if (/Mobi|Android/i.test(navigator.userAgent)) {
-    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 1.5rem)`)
+
+
+if (window.innerHeight > window.innerWidth) {
+    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
     body.classList.add("contracted")
 }
 
 button.addEventListener("click", e => {
-    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 1.5rem)`)
+    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
     body.classList.toggle("contracted")
 })
