8fe14d43adf590352c51b890081cade0fac2506d
diademiemi <emilia@diademiemi.me>
2022-02-19 13:36:57 +0100

    make sidebar slightly larger

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let sidebar = document.getElementsByClassName("sidebar")[0]
 let body = document.getElementById("body")
 
 if (/Mobi|Android/i.test(navigator.userAgent)) {
-    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
+    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 1.5rem)`)
     body.classList.add("contracted")
 }
 
 button.addEventListener("click", e => {
-    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
+    body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 1.5rem)`)
     body.classList.toggle("contracted")
 })
