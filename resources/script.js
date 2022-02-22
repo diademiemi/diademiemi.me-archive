eeb4682214f7831e23ef597e59dfb0b622fda896
diademiemi <emilia@diademiemi.me>
2022-02-22 22:14:45 +0100

    Add THE SUSSENING

--- resources/script.js
+++ resources/script.js
@@ -2,16 +2,95 @@ let button = document.getElementById("collapse")
 let sidebar = document.getElementsByClassName("sidebar")[0]
 let body = document.getElementById("body")
 
-
-
-window.onload = () => {
-    if (window.innerHeight > window.innerWidth) {
-        body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
-        body.classList.toggle("contracted")
-    }
-}
-
+/**
+ * Sidebar toggle button
+ */
 button.addEventListener("click", e => {
     body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
     body.classList.toggle("contracted")
 })
+
+// The fuckening.
+let soundPlayer;
+let buffer = ["", "", "", "", ""];
+let tracker = 0;
+
+// The sussy style sheet
+let sussyStyle = `body {
+        background-color: #00000000;
+        background-image: url(../resources/eggs/drip.jpg);
+        background-position-y: 30%;
+        background-size: cover;
+    }
+    
+    .sidebar {
+        background-color: #ff0000;
+    }
+
+    .sidebar a {
+        color: #ffff00;
+    }
+
+    .sidebar .header {
+        color: #ffff00;
+    }
+
+    .box .footer {
+        background-color: #00ffff;
+    }`;
+
+// The sussy song file
+let sussySong = "../resources/eggs/drip.mp3"
+
+/**
+ * Create stylesheet
+ */
+let styleSheet = document.createElement("style");
+
+/**
+ * Listen for keystrokes and match with patterns
+ * 
+ * @param event key event
+ */
+function keyEvent(event) {
+    buffer.shift();
+    buffer.push(event.key);
+
+    // Check if buffer includes sus
+    if (buffer.join("").includes("sus")) {
+        if (tracker !== 1) {
+            console.log("Sus mode engaged");
+            soundPlayer.src = sussySong;
+            soundPlayer.loop = true;
+            soundPlayer.load();
+            soundPlayer.play();
+            styleSheet.innerText = sussyStyle;
+            document.head.appendChild(styleSheet);
+            buffer = ["", "", "", "", ""];
+            tracker = 1;
+        } else {
+            console.log("Sus mode disengaged");
+            soundPlayer.pause();
+            document.head.removeChild(styleSheet);
+            buffer = ["", "", "", "", ""];
+            tracker = 0;
+        }
+    }
+}
+
+// Initialise listener
+ function listenerInit() {
+    soundPlayer = document.querySelector("#player");
+    document.addEventListener("keypress", keyEvent);
+}
+
+
+window.onload = () => {
+    // Check if sidebar should start contracted
+    if (window.innerHeight > window.innerWidth) {
+        body.style.setProperty("--hide", `calc(-${sidebar.clientWidth}px + 2rem)`)
+        body.classList.toggle("contracted")
+    }
+    // Init listener
+    listenerInit();
+}
