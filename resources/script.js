c75935c88441f97cc8a0be59cb1d9a576011fd54
diademiemi <emilia@diademiemi.me>
2022-02-23 19:59:50 +0100

    Hey guys, did you know that in terms of

--- resources/script.js
+++ resources/script.js
@@ -47,7 +47,6 @@ let skellyStyle = `body {
     background-color: #00000000;
     background-image: url(../resources/eggs/skeleton.gif);
     background-position-y: 30%;
-    background-position-x: 80%;
     background-size: cover;
 }
 
@@ -70,6 +69,35 @@ let skellyStyle = `body {
 // The sussy song file
 let skellySong = "../resources/eggs/skeleton.mp3"
 
+// This will cost me my job and any chances of future employment.
+let vaporeonStyle = `body {
+    background-color: #ffffff;
+    background-image: url(../resources/eggs/vaporeon.png);
+    background-position-y: 30%;
+    background-size: cover;
+}
+
+.sidebar {
+    background-color: #336f8d;
+}
+
+.sidebar a {
+    color: #ffffff;
+}
+
+.sidebar .header {
+    color: #ffffff;
+}
+
+.box .footer {
+    background-color: #78cce0;
+}
+
+.box .main p {
+    font-size: 2.5rem;
+    color: #000000;
+}
+`;
 
 /**
  * Create stylesheet
@@ -119,6 +147,30 @@ function keyEvent(event) {
             console.log("THE SKELETON DISAPPEARS");
             soundPlayer.pause();
             document.head.removeChild(styleSheet);
+            buffer = ["", "", "", "", "", "", "", ""];
+            tracker = 0;
+        }
+    } else if (buffer.join("").includes("vaporeon")) {
+        if (tracker !== 2) {
+            console.log("Hey guys");
+            soundPlayer.pause();
+            styleSheet.innerText = vaporeonStyle;
+            document.head.appendChild(styleSheet);
+            document.getElementById("box").innerHTML = `
+            <div class="main">
+                <p>  Hey guys, did you know that in terms of male human and female Pokémon breeding, Vaporeon is the most compatible Pokémon for humans? Not only are they in the field egg group, which is mostly comprised of mammals, Vaporeon are an average of 3”03’ tall and 63.9 pounds, this means they’re large enough to be able handle human dicks, and with their impressive Base Stats for HP and access to Acid Armor, you can be rough with one. Due to their mostly water based biology, there’s no doubt in my mind that an aroused Vaporeon would be incredibly wet, so wet that you could easily have sex with one for hours without getting sore. They can also learn the moves Attract, Baby-Doll Eyes, Captivate, Charm, and Tail Whip, along with not having fur to hide nipples, so it’d be incredibly easy for one to get you in the mood. With their abilities Water Absorb and Hydration, they can easily recover from fatigue with enough water. No other Pokémon comes close to this level of compatibility. Also, fun fact, if you pull out enough, you can make your Vaporeon turn white. Vaporeon is literally built for human dick. Ungodly defense stat+high HP pool+Acid Armor means it can take cock all day, all shapes and sizes and still come for more
+                </p>
+            </div>
+            <div class="footer">
+                <p id="collapse"></p>
+            </div>
+            `
+            buffer = ["", "", "", "", "", "", "", ""];
+            tracker = 2;
+        } else {
+            console.log("come for more fun facts!");
+            document.head.removeChild(styleSheet);
+            location.reload(true);
             buffer = ["", "", "", "", "", "", "", ""];
             tracker = 0;
         }
